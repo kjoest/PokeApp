09d3ef30882d3ca44e89d973d76e4235b519c635
kyle Joest <kjoest92@gmail.com>
2023-12-09 17:29:31 -0600

    Added Strenghs and Weaknesses

--- script.pokemon.js
+++ script.pokemon.js
@@ -14,6 +14,14 @@ const TYPES = [
     'dragon', 'dark', 'fairy'
 ];
 
+//Function to fetch type details
+async function fetchTypeDetails(typeName) {
+    const apiUrl = `https://pokeapi.co/api/v2/type/${typeName.toLowerCase()}`;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    return data;
+}
+
 const resetCard = () => {
     mainCard.classList.remove('hide');
     for (const type of TYPES) {
@@ -31,11 +39,11 @@ const resetBody = () => {
 let pokemon = {
     fetchPokemon: function (name, id) {
         Promise.all([
-            fetch("https://pokeapi.co/api/v2/" + "pokemon/" + name.toLowerCase()).then((response) => response.json()).then((data) => this.displayPokemon(data))
-            /*fetch("https://pokeapi.co/api/v2/" + "type/1").then((response) => response.json()).then((data) => this.displayPokemon(data)),*/
+            fetch("https://pokeapi.co/api/v2/" + "pokemon/" + name.toLowerCase()).then((response) => response.json()).then((data) => this.displayPokemon(data)),
         ]);
     },
-    displayPokemon: function (data) {
+
+    displayPokemon: async function (data) {
 
         resetCard();
         resetBody();
@@ -69,15 +77,32 @@ let pokemon = {
         document.querySelector(".poke-id").innerText = "ID: " + id;
         document.querySelector(".poke-height").innerText = "Height: " + height;
         document.querySelector(".poke-weight").innerText = "Weight: " + weight;
+
+    // Fetch and display weaknesses and strengths
+    const type1Details = await fetchTypeDetails(dataFirstType['type']['name']);
+    const type1Weaknesses = type1Details.damage_relations.double_damage_from.map(type => type.name);
+    const type1Strengths = type1Details.damage_relations.double_damage_to.map(type => type.name);
+
+    let type2Weaknesses = [];
+    let type2Strengths = [];
+
+    if (dataSecondType) {
+        const type2Details = await fetchTypeDetails(dataSecondType['type']['name']);
+        type2Weaknesses = type2Details.damage_relations.double_damage_from.map(type => type.name);
+        type2Strengths = type2Details.damage_relations.double_damage_to.map(type => type.name);
+    }
+
+    const allWeaknesses = [...new Set([...type1Weaknesses, ...type2Weaknesses])];
+    const allStrengths = [...new Set([...type1Strengths, ...type2Strengths])];
+
+    // Update HTML with weaknesses and strengths
+    weakAgainst.textContent = `Weak Against: ${allWeaknesses.join(', ')}`;
+    strongAgainst.textContent = `Strong Against: ${allStrengths.join(', ')}`;
+
+    // Show the card after updating details
+    mainCard.classList.remove('hide');
+
     },
-    /*displayPokemonTypes: function(data) {
-        const dataDamageTypes = data['damage_relations']['double_damage_to'];
-        const dataFirstDamageType = dataDamageTypes[0];
-        const dataSecondDamageType = dataDamageTypes[1];
-        strongAgainst.textContent = dataFirstDamageType['name'];
-        weakAgainst.textContext = dataDamageTypes['name'];
-        console.log(dataFirstDamageType);
-    },*/
 
     search: function () {
         this.fetchPokemon(document.querySelector(".search-bar").value);
